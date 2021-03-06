909c7b85827a0b4b0772ef7d593c20890a5039eb
unknown <loaki.dev@gmail.com>
2021-03-06 11:20:26 +0100

    ok

--- setup.sh
+++ setup.sh
@@ -2,37 +2,99 @@
 # Remove the old SSH certificate in the IMAC
 # ssh-keygen -R 192.168.99.103
 
-echo "Deleting minikube..."
-    minikube delete
+services_start=`date +%s`
+rm -f last.log
+printf "##########################################\n\
+                Minikube\n\
+##########################################\n" >> last.log
+printf "\e[93mStarting minikube...\e[0m\r"
+start=`date +%s`
+minikube delete >> last.log
+minikube start --driver=virtualbox --no-vtx-check --memory='3072' >> last.log
+end=`date +%s`
+runtime=$((end-start))
+printf "\e[K\e[92mStarting minikube DONE (in ${runtime}s)\e[0m\n"
 
-echo "Starting minikube..."
-    #minikube start --driver=virtualbox --memory='3072'
-    minikube start --vm-driver=docker --memory=3072mb
-    minikube dashboard &
-
-echo "Install MetalLB..."
-    minikube addons enable metallb
+printf "##########################################\n\
+                MetalLB\n\
+##########################################\n" >> last.log
+printf "\e[93mInstalling MetalLB...\e[0m\r"
+    start=`date +%s`
+    minikube addons enable metallb >> last.log
     # The memberlist secret contains the secretkey to encrypt the communication between speakers for the fast dead node detection.
-    kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
+    kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)" >> last.log
     # Deploy MetalLB to the cluster
-    kubectl apply -f ./srcs/yamls/metallb.yaml
+    kubectl apply -f ./srcs/yamls/metallb.yaml >> last.log
+    end=`date +%s`
+    runtime=$((end-start))
+    printf "\e[K\e[92mInstalling MetalLB DONE (in ${runtime}s)\e[0m\n"
 
-echo "Building images..."
-    #eval $(minikube docker-env) # https://stackoverflow.com/questions/52310599/what-does-minikube-docker-env-mean
-    eval $(minikube -p minikube docker-env --shell=bash)
-    docker build -t nginx_service ./srcs/nginx
-    docker build -t mysql_service ./srcs/mysql
-    docker build -t wordpress_service ./srcs/wordpress
-    docker build -t phpmyadmin_service ./srcs/phpmyadmin
-    docker build -t ftps_service ./srcs/ftps
-    docker build -t influxdb_service ./srcs/influxdb
-    docker build -t grafana_service ./srcs/grafana
+function build_spec() 
+{
+    printf "##########################################\n\
+                ${1}\n\
+##########################################\n" >> last.log
+	printf "\e[93mSTART BUILDING ${1}...\e[0m\r"
+	start=`date +%s`
+	docker build -t $1_image ./srcs/$1 >> last.log
+	end=`date +%s`
+	runtime=$((end-start))
+	printf "\e[K\e[92m${1} DONE (in ${runtime}s)\e[0m\n"
+}
 
-echo "Creating pods and services..."
-    kubectl apply -f ./srcs/yamls/nginx.yaml
-    kubectl apply -f ./srcs/yamls/mysql.yaml
-    kubectl apply -f ./srcs/yamls/wordpress.yaml
-    kubectl apply -f ./srcs/yamls/phpmyadmin.yaml
-    kubectl apply -f ./srcs/yamls/ftps.yaml
-    kubectl apply -f ./srcs/yamls/influxdb.yaml
-    kubectl apply -f ./srcs/yamls/grafana.yaml
+eval $(minikube -p minikube docker-env --shell=bash)
+build_spec influxdb
+build_spec mysql
+build_spec nginx
+build_spec phpmyadmin
+printf "\e[36mPlease wait 10 sec...\e[0m\r"
+sleep 10
+build_spec wordpress
+build_spec ftps
+build_spec grafana
+
+printf "\e[93mCreating pods and services...\e[0m\r"
+    start=`date +%s`
+    kubectl apply -f ./srcs/yamls/nginx.yaml >> last.log
+    kubectl apply -f ./srcs/yamls/mysql.yaml >> last.log
+    kubectl apply -f ./srcs/yamls/wordpress.yaml >> last.log
+    kubectl apply -f ./srcs/yamls/phpmyadmin.yaml >> last.log
+    kubectl apply -f ./srcs/yamls/ftps.yaml >> last.log
+    kubectl apply -f ./srcs/yamls/influxdb.yaml >> last.log
+    kubectl apply -f ./srcs/yamls/grafana.yaml >> last.log
+    end=`date +%s`
+    runtime=$((end-start))
+    printf "\e[K\e[92mCreating pods and services DONE (in ${runtime}s)\e[0m\n"
+
+end=`date +%s`
+runtime=$((end-services_start))
+printf "\e[36mServices DONE (in ${runtime}s)\e[0m\n"
+minikube dashboard &
+
+#printf "\e[93mBuilding images...\e[0m\r"
+#    start=`date +%s`
+#    #eval $(minikube docker-env) # https://stackoverflow.com/questions/52310599/what-does-minikube-docker-env-mean
+#    eval $(minikube -p minikube docker-env --shell=bash)
+#    docker build -t nginx_service ./srcs/nginx >> last.log
+#    docker build -t mysql_service ./srcs/mysql >> last.log
+#    docker build -t wordpress_service ./srcs/wordpress >> last.log
+#    docker build -t phpmyadmin_service ./srcs/phpmyadmin >> last.log
+#    docker build -t ftps_service ./srcs/ftps >> last.log
+#    docker build -t influxdb_service ./srcs/influxdb >> last.log
+#    docker build -t grafana_service ./srcs/grafana >> last.log
+#    end=`date +%s`
+#    runtime=$((end-start))
+#    printf "\e[K\e[92mBuilding images DONE (in ${runtime}s)\e[0m\n"
+
+#printf "\e[93mCreating pods and services...\e[0m\r"
+#    start=`date +%s`
+#    kubectl apply -f ./srcs/yamls/nginx.yaml >> last.log
+#    kubectl apply -f ./srcs/yamls/mysql.yaml >> last.log
+#    kubectl apply -f ./srcs/yamls/wordpress.yaml >> last.log
+#    kubectl apply -f ./srcs/yamls/phpmyadmin.yaml >> last.log
+#    kubectl apply -f ./srcs/yamls/ftps.yaml >> last.log
+#    kubectl apply -f ./srcs/yamls/influxdb.yaml >> last.log
+#    kubectl apply -f ./srcs/yamls/grafana.yaml >> last.log
+#    end=`date +%s`
+#    runtime=$((end-start))
+#    printf "\e[K\e[92mCreating pods and services DONE (in ${runtime}s)\e[0m\n"
